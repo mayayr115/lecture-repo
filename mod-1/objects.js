047f9672b1681b3e7b70b2d431dbc2ff16c654c1
mayayr115 <mayaramkishuncsh2027@gmail.com>
2023-10-18 15:43:29 -0400

    Updated objects.js

--- mod-1/objects.js
+++ mod-1/objects.js
@@ -9,3 +9,21 @@ car.color = 'blue'; //dot notation, used for object properties
 car['model-year'] = 2018 //bracket notation, used for variables and object property strings
 console.log(car.name)
 console.log(car[practice])
+
+//Lab Review
+const userBen = {
+  name: 'ben',
+  age: 28,
+  canDrive: true,
+  friends: ['maya', 'reuben', 'zo']
+}
+
+const names = ['wendy', 'jon', 'daniel']
+const [mom, dad, brother] = names //DESTRUCTURING!!
+console.log(mom, dad, brother)
+
+//destructuring in a FUNCTION! (array nolaCoords = 3 parameters in the array of printCoord parameter)
+const nolaCoords = [33, 90, 'New Orleans']
+const printCoords = ([lat, long, city]) => {
+  return `Latitude: ${lat}, Longitude: ${long}, City: ${city}`
+}
