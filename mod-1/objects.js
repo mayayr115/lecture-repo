799d2c52766f9ea3d3ec933d41b171496ed32a6e
mayayr115 <mayaramkishuncsh2027@gmail.com>
2023-10-18 13:12:16 -0400

    Added objects.js

---
+++ mod-1/objects.js
@@ -0,0 +1,11 @@
+const car = {
+  name: 'Camry',
+  maker: 'Toyota',
+}
+// console.log(car)
+
+const practice = 'name' //'name' is object's property name!
+car.color = 'blue'; //dot notation, used for object properties
+car['model-year'] = 2018 //bracket notation, used for variables and object property strings
+console.log(car.name)
+console.log(car[practice])
